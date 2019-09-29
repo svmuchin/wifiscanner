8500d8ab95a91729048d4d6704ffb41a7ede2fd9
Sharnin Artem <shao_sharnin@mail.ru>
2019-09-29 16:45:06 +0300

    Фикс отображения списка

--- app/src/main/java/com/wifi/wifiscanner/StubReport.java
+++ app/src/main/java/com/wifi/wifiscanner/StubReport.java
@@ -22,7 +22,17 @@ public class StubReport extends Report {
     private List<Result> initResults() {
         ArrayList<Result> results = new ArrayList<>();
         results.add(initResult("BAC9I_WIFI", "172.173.23.42", 1, 1));
+        results.add(initResult("PIVNUSHKA_FREE_WIFI", "172.11.228.19", 1, 1));
         results.add(initResult("PIVNUSHKA_FREE_WIFI", "172.11.228.19", 2, 2));
+        results.add(initResult("PIVNUSHKA_FREE_WIFI", "172.11.228.19", 3, 3));
+        results.add(initResult("PIVNUSHKA_FREE_WIFI", "172.11.228.19", 4, 4));
+        results.add(initResult("PIVNUSHKA_FREE_WIFI", "172.11.228.19", 5, 5));
+        results.add(initResult("PIVNUSHKA_FREE_WIFI", "172.11.228.19", 6, 6));
+        results.add(initResult("BAC9I_WIFI", "172.173.23.42", 2, 2));
+        results.add(initResult("BAC9I_WIFI", "172.173.23.42", 3, 3));
+        results.add(initResult("BAC9I_WIFI", "172.173.23.42", 4, 4));
+        results.add(initResult("BAC9I_WIFI", "172.173.23.42", 5, 5));
+        results.add(initResult("MIKSHIN_FOREVA", "00:0e:8f:d3:5b:e4", 6, 6));
         return results;
     }
 
