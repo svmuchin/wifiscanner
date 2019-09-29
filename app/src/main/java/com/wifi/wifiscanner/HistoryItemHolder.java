0fb752a7a619aaa1d63441bbc93972a85a0337e5
Sharnin Artem <shao_sharnin@mail.ru>
2019-09-29 19:15:45 +0300

    Добавление лидирующих нулей при выводе времени и даты в историю

--- app/src/main/java/com/wifi/wifiscanner/HistoryItemHolder.java
+++ app/src/main/java/com/wifi/wifiscanner/HistoryItemHolder.java
@@ -20,9 +20,16 @@ public class HistoryItemHolder extends RecyclerView.ViewHolder {
 
     public void bind(Report report) {
         Calendar calendar = report.getCalendar();
-        this.date.setText(String.format("%s.%s.%s", calendar.get(Calendar.DAY_OF_MONTH),
-                calendar.get(Calendar.MONTH), calendar.get(Calendar.YEAR)));
-        this.time.setText(String.format("%s:%s", calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE)));
+        this.date.setText(String.format("%s.%s.%s", this.addLeadingZero(calendar.get(Calendar.DAY_OF_MONTH)),
+                this.addLeadingZero(calendar.get(Calendar.MONTH)), calendar.get(Calendar.YEAR)));
+        this.time.setText(String.format("%s:%s", this.addLeadingZero(calendar.get(Calendar.HOUR_OF_DAY)),
+                this.addLeadingZero(calendar.get(Calendar.MINUTE))));
+    }
+
+    private String addLeadingZero(int i) {
+        if (i < 10) {
+            return "0" + i;
+        }
+        return Integer.toString(i);
     }
 }
