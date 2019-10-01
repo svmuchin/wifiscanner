4039957bbf08558e5a20427af6c03ec050cdf665
svmuchin <4>
2019-10-01 07:30:42 +0300

    Тест asyncTask

--- app/src/main/java/com/wifi/wifiscanner/SimpleStorage.java
+++ app/src/main/java/com/wifi/wifiscanner/SimpleStorage.java
@@ -6,43 +6,46 @@ import java.util.List;
 
 public class SimpleStorage implements Storage {
 
-    private static SimpleStorage SIMPLE_STORAGE = new SimpleStorage();
+  private static SimpleStorage instance;
 
-    private List<Report> reports = new ArrayList<>();
+  private List<Report> reports = new ArrayList<>();
 
-    public static SimpleStorage getStorage() {
-        return SIMPLE_STORAGE;
+  public static SimpleStorage getStorage() {
+    if (instance == null) {
+      instance = new SimpleStorage();
     }
+    return instance;
+  }
 
-    @Override
-    public Report get(String key) {
-        for (Report report : this.reports) {
-            if (report.getId().equals(key)) {
-                return report;
-            }
-        }
-        return null;
+  @Override
+  public Report get(String key) {
+    for (Report report : this.reports) {
+      if (report.getId().equals(key)) {
+        return report;
+      }
     }
+    return null;
+  }
 
-    @Override
-    public List<Report> getAll() {
-        return this.reports;
-    }
+  @Override
+  public List<Report> getAll() {
+    return this.reports;
+  }
 
-    @Override
-    public void save(Report report) {
-        this.reports.add(report);
-    }
+  @Override
+  public void save(Report report) {
+    this.reports.add(report);
+  }
 
-    @Override
-    public void remove(Report report) {
-        Iterator<Report> iterator = this.reports.iterator();
-        while (iterator.hasNext()) {
-            Report next = iterator.next();
-            if (next.getId().equals(report.getId())) {
-                iterator.remove();
-                return;
-            }
-        }
+  @Override
+  public void remove(Report report) {
+    Iterator<Report> iterator = this.reports.iterator();
+    while (iterator.hasNext()) {
+      Report next = iterator.next();
+      if (next.getId().equals(report.getId())) {
+        iterator.remove();
+        return;
+      }
     }
+  }
 }
