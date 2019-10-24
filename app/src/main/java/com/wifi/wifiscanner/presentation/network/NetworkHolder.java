8bc8e72e2d43de9ce42e60e3d6b16e7a0491f2c4
Зарубин Михаил <none.zma@gmail.com>
2019-10-24 21:48:59 +0300

    Красим сигнал в зависимости от мощи.

--- app/src/main/java/com/wifi/wifiscanner/presentation/network/NetworkHolder.java
+++ app/src/main/java/com/wifi/wifiscanner/presentation/network/NetworkHolder.java
@@ -1,15 +1,18 @@
 package com.wifi.wifiscanner.presentation.network;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.wifi.wifiscanner.R;
-
 import com.wifi.wifiscanner.dto.AccessPoint;
 
 public class NetworkHolder extends RecyclerView.ViewHolder {
+
+    private static final int WEAK_SIGNAL = -90;
+    private static final int STRONG_SIGNAL = -30;
 
     private TextView name;
     private TextView signal;
@@ -44,6 +47,18 @@ public class NetworkHolder extends RecyclerView.ViewHolder {
         this.name.setText(networkInfo.getSSID());
         this.address.setText(networkInfo.getBSSID());
         this.signal.setText(Integer.toString(networkInfo.getRSSI()));
+        this.signal.setBackgroundColor(this.calculateColor(networkInfo.getRSSI()));
         this.channel.setText(Integer.toString(networkInfo.getChannel()));
+    }
+
+    private int calculateColor(int rssi) {
+        float signal = Math.max(Math.min(rssi, STRONG_SIGNAL), WEAK_SIGNAL);
+
+        float percent = (signal - WEAK_SIGNAL) / (STRONG_SIGNAL - WEAK_SIGNAL);
+
+        int resultRed = Math.round(255 - percent * 255);
+        int resultGreen = Math.round(percent * 255);
+
+        return Color.rgb(resultRed, resultGreen, 0);
     }
 }
